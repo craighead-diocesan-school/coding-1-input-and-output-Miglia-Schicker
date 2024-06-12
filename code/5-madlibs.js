488b9c6e51e603ec0dc0520253f35e1dc9c36ec4
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-06-13 10:14:07 +1200

    madlibs

--- code/5-madlibs.js
+++ code/5-madlibs.js
@@ -3,5 +3,10 @@
 // ####################################
 
 function madLibs() {
+    let exclamation = prompt ('Give me an exclamation')
+    let adverb = prompt ('Give me an adverb')
+    let noun = prompt ('Give me a noun')
+    let adjective = prompt ('Give me an adjective')
+    alert (exclamation + '! He said ' + adverb + ' as he jumped into his convertible ' + noun + ' and drove off with his ' + adjective + ' wife. ')
   
 }
