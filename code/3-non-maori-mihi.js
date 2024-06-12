4acb92441214a88568f112b049f2584a00fbf545
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-06-12 15:12:59 +1200

    Non Maori Mihi

--- code/3-non-maori-mihi.js
+++ code/3-non-maori-mihi.js
@@ -7,13 +7,20 @@ function nonMaoriMihi() {
   alert('This program will help you build a basic mihi.')
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
-  alert('What is your name?')
-  let name = prompt()
-  // more questions and answers here
+ 
+  let name = prompt('What is your name?')
+  let mountain = prompt('What mountain do you identify with?')
+  let river = prompt('What river do you identify with')
+  let hometown =prompt('What is your hometown?')
+  
 
   alert('Your mihi:')
-  // more outputs here
-
+  alert('Tēnā koutou')
+  alert('Kei te mihi au ki a ' + mountain + ',')
+  alert('Kei te mihi su ki a ' + river + ',')
+  alert('Ko Te Titiri o Waitangi te waka,')
+  alert('Ko te Aotearoa te iwe,')
+  alert('No ' + hometown + ' au')
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
