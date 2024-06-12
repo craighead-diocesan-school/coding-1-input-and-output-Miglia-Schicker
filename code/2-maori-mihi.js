dae62d007606bc1ff12619a95f7b420186f3c901
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-06-12 15:03:56 +1200

    Maori Mihi

--- code/2-maori-mihi.js
+++ code/2-maori-mihi.js
@@ -7,13 +7,18 @@ function maoriMihi() {
   alert('This program will help you build a basic mihi.')
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
-  alert('What is your name?')
-  let name = prompt()
-  // more questions and answers here
+
+  let name = prompt('What is your name?')
+  let mountain = prompt('What is your mountain?')
+  let river = prompt('What is your river?')
+  let hometown = prompt('What is your hometown?')
+
 
   alert('Your mihi:')
-  // more outputs here
-
+  alert('Tēnā koutou katoa,')
+  alert('Ko ' + mountain + ' tōku ingoa,')
+  alert('Ko ' + river + ' tōku awa,')
+  alert('Ko ' + hometown + ' au,')
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
